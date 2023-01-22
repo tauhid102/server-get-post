adeb71924a7120bec371196cdb524c613e0a9dd6
Tauhidur Rahman <79333795+tauhid102@users.noreply.github.com>
2023-01-22 13:31:31 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
