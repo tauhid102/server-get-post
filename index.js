82ce156a1d3241bdcf1fa9774a9ba50f8f06ae94
Tauhidur Rahman <79333795+tauhid102@users.noreply.github.com>
2023-01-22 13:29:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfvuq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('bookShop');
+        const userCollection = database.collection('users');
+
+        // fetch by name
+        app.get('/get_name', async (req, res) => {
+            const email = req.query.name;
+            const query = { name: name }
+            const cursor = userCollection.find(query);
+            const buy = await cursor.toArray();
+            res.json(buy);
+        });
+        //save name
+        app.post('/save_user', async (req, res) => {
+            const cursor = req.body;
+            const result = await userCollection.insertOne(cursor);
+            res.json(result);
+        });
+    }
+    finally {
+        //a
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server Start');
+});
+app.listen(port, () => {
+    console.log('Listening to port', port)
+})
+
+
